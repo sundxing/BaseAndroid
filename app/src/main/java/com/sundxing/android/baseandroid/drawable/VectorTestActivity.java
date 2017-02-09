59fe7c6b730cb224faf6e38ed0aa2c9041dcd0ce
duanxingsun <duanxing.sun@ihandysoft.com>
2017-02-09 20:16:16 +0800

    add demo for vector hint mode

--- app/src/main/java/com/sundxing/android/baseandroid/drawable/VectorTestActivity.java
+++ app/src/main/java/com/sundxing/android/baseandroid/drawable/VectorTestActivity.java
@@ -1,5 +1,6 @@
 package com.sundxing.android.baseandroid.drawable;
 
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,7 +8,6 @@ import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -15,11 +15,26 @@ import android.widget.TextView;
 
 import com.sundxing.android.baseandroid.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by sundxing on 17/1/18.
  */
 
 public class VectorTestActivity extends AppCompatActivity {
+
+   static Map<String, Integer> resMap = new HashMap<>();
+    static {
+        resMap.put("src_in", R.drawable.vector_hint_color);
+        resMap.put("add", R.drawable.vector_hint_color_add);
+        resMap.put("screen", R.drawable.vector_hint_color_screen);
+        resMap.put("multiply", R.drawable.vector_hint_color_muti);
+        resMap.put("src_over", R.drawable.vector_hint_color_over);
+        resMap.put("src_atop", R.drawable.vector_hint_color_atop);
+        resMap.put("原图", R.drawable.vector_hint);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,12 +69,31 @@ public class VectorTestActivity extends AppCompatActivity {
                 drawableClickBlue);
         tv.setClickable(true);
 
+        TextView colorTv = new TextView(this);
+        colorTv.setBackgroundColor(Color.parseColor("#7700cc00"));
+        colorTv.setText("Tint Color : #7700cc00");
+        colorTv.setPadding(20,20,20,20);
+
         ScrollView container = new ScrollView(this);
         LinearLayout l = new LinearLayout(this);
         l.setOrientation(LinearLayout.VERTICAL);
-        l.addView(textView, 60, 60);
-        l.addView(tv, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//        container.addView(l);
-        setContentView(l);
+        l.addView(colorTv);
+        fill(l);
+        l.addView(getLayoutInflater().inflate(R.layout.layout_png_tint, l, false));
+        container.addView(l);
+        setContentView(container);
+    }
+
+    private void fill(LinearLayout l) {
+        for (Map.Entry<String, Integer> entry: resMap.entrySet()) {
+            TextView tv = new TextView(this);
+            tv.setText(entry.getKey());
+
+            tv.setCompoundDrawablesWithIntrinsicBounds(VectorDrawableCompat.create(getResources(), entry.getValue(), null),
+                    null, null, null);
+            tv.setClickable(true);
+            l.addView(tv);
+        }
+
     }
 }
