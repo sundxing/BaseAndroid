427cb3039ee2eb7ba5eb3a1581c99769e5ca5d34
duanxingsun <duanxing.sun@ihandysoft.com>
2017-02-10 11:05:10 +0800

    add drawable getter util, compact vetor png format.

---
+++ app/src/main/java/com/sundxing/android/baseandroid/util/BitmapUtil.java
@@ -0,0 +1,29 @@
+package com.sundxing.android.baseandroid.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatDrawableManager;
+
+/**
+ * Created by sundxing on 17/2/9.
+ */
+
+public class BitmapUtil {
+    // get Drawable for res id.
+    public static Drawable getDrawable(Context context, int resId) {
+        return AppCompatDrawableManager.get().getDrawable(context, resId);
+    }
+
+    public static Bitmap getBitmap(Context context, int resId) {
+        Drawable drawable = getDrawable(context, resId);
+        Bitmap mBadgeBitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(),
+                Bitmap.Config.ARGB_4444);
+        Canvas canvas = new Canvas(mBadgeBitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return mBadgeBitmap
+    }
+
+}
